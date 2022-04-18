c778f13fdb897ef150f68c3d2ae4928fd69d3cc8
OdidiLavender <odidilavender@gmail.com>
2022-04-19 02:14:13 +0300

    modified the github services file

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-// import { repos } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
@@ -11,19 +10,23 @@ export class GithubService {
   // private =client_secret:`3d3487c52a77d4bb6cd49cb665314e560766e530`;
   
   url : string = "https://api.github.com/users/"
-   repo : string =  "https://api.github.com/users/username/repos"
+   repos : string =  "https://api.github.com/users/username/repos"
   constructor(private http:HttpClient ) {
 
    }
    getUser( username: string)
-  //  details(repo:string)
+ 
   {
   //   https://api.github.com/users/username
   //   https://api.github.com/users/username/repos
 
   return this.http.get(this.url + username );
-  // return this.http.get(this.repo + this.details)
   
+  
+}
+
+getRepo (username:string){
+  return this.http.get(this.url + username + "/repos");
 }
   
 
