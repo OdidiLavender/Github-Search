2c6cbde575d5bc33a881471b090eff84b7ce2c52
OdidiLavender <odidilavender@gmail.com>
2022-04-18 22:50:51 +0300

    did the routing to the file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SearchpageComponent } from './components/searchpage/searchpage.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+     path: '',
+     component: HomeComponent
+},
+{
+     path:'search',
+     component: SearchpageComponent
+},
+{
+  path:'user/:id',
+  component: UserDetailsComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
