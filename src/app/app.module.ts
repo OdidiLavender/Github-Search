02fa3f664d043e85f8f0bd3a4d4e08ebc08c1cb9
OdidiLavender <odidilavender@gmail.com>
2022-04-18 22:46:25 +0300

    modified the app module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchpageComponent } from './components/searchpage/searchpage.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { HttpClientModule} from '@angular/common/http'
+// import { repos } from '@angular/common/http'
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SearchpageComponent,
+    UserDetailsComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    // details
   ],
   providers: [],
   bootstrap: [AppComponent]
